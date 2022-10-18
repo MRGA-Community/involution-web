b559874b1de4e68a0fff52982a00913150fba642
Slimer210 <howweishan0210@outlook.com>
2022-10-18 18:55:33 +0800

    Dashboard Update
    Co-authored-by: Melvin Chia <melvinchia623600@gmail.com>
    Co-authored-by: mokyeah <mokyeah@users.noreply.github.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#96B7DA",
+        secondary: "#F3F3F3",
+        black: "#525252",
+      },
+      boxShadow: {
+        container: "0px 4px 8px rgba(0,0,0,0.10)"
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
